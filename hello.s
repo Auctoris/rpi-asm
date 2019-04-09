3bbba93891eacb5a63054aff5e8a087faf05a347
AJP <ajp@auctoris.co.uk>
2019-04-09 13:37:39 +0000

    Change literals to use explicit hexidecimal notation

--- hello.s
+++ hello.s
@@ -1,13 +1,13 @@
 .global _start
 _start:
-    MOV R7, #4
-    MOV R0, #1
-    MOV R2, #12
+    MOV R7, #0x04
+    MOV R0, #0x01
+    MOV R2, #0x0D
     LDR R1, =string
     SWI 0
-    MOV R7, #1
-    MOV R0, #0
+    MOV R7, #0x01
+    MOV R0, #0x00
     SWI 0
     .data
 string:
-    .ascii "Hello World\n"
+    .ascii "Hello World!\n"
